9325a92f267ea9bb57147083216bd83605ca8b86
htevicayola29 <177040461+htevicayola29@users.noreply.github.com>
2025-10-13 11:23:04 -0400

    Proyecto Places

---
+++ android/app/src/main/java/com/htevi/places/MainActivity.java
@@ -0,0 +1,6 @@
+package com.htevi.places;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/description_place.dart
@@ -0,0 +1,83 @@
+
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget{
+  // variables
+  String textoTitulo;
+  int cantidadEstrellas;
+  String textoDescription;
+  DescriptionPlace(this.textoTitulo,this.cantidadEstrellas,this.textoDescription);
+  @override
+  Widget build(BuildContext context) {
+    final titutlo = Container(
+      margin: EdgeInsets.only(
+        right: 20
+      ),
+      child: Text(
+        textoTitulo,
+        style: TextStyle(
+          fontFamily: "Lato",
+          fontSize: 38,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+   final estrella=Container(
+     margin: EdgeInsets.only(
+       right: 5
+     ),
+     child:Icon(
+       Icons.star,
+       color: Colors.amber,
+     ),
+   );
+    final estrellaBorde=Container(
+      margin: EdgeInsets.only(
+        right: 5
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Colors.black54,
+      ),
+    );
+    //fila estrellas
+    List<Container>estrellas=[];
+    for(int i=0;i<5;i++){
+      if(i<cantidadEstrellas){
+        estrellas.add(estrella);
+      }else {
+        estrellas.add(estrellaBorde);
+      }
+
+    }
+   final filaEstrellas= Row(
+     children:estrellas,
+   );
+   final filaTitulo=Row(
+     children: <Widget>[
+       titutlo,
+       filaEstrellas
+     ],
+   );
+   final description=Container(
+     margin: EdgeInsets.only(
+       top: 10
+     ),
+     child: Text(
+       textoDescription,
+       style: TextStyle(
+         fontFamily: "Lato",
+         color: Colors.black54
+     ),
+     ),
+   );
+    final descriptionPlace =Column(
+      children:<Widget>[
+        filaTitulo,
+        description
+        ],
+    );
+    return descriptionPlace;
+  }
+
+}
